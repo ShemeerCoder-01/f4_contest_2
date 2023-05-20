aa18268831d83390e72737ce3369f96dd883d30d
Shemeer <shameerbem@gmail.com>
2023-05-20 17:08:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import Row from "./components/Row";
 const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 function App() {
-
   let [arr, setArr] = useState([]);
-console.log(arr);
+  
   const fetchData = async (url) => {
     try {
       const res = await fetch(url);
       const data = await res.json();
       setArr(data);
-
-
     }
     catch (e) {
       console.log("Error", e);
